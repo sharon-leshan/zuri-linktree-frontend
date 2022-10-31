ac85ddbacddd67a1f76dd785af870a31ce50e015
sharon-leshan <maleos380@gmail.com>
2022-10-31 17:53:46 +0300

    App update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { profile, share } from './assets';
+import { Footer, Links, Profile } from './components';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-     Header
+    <div className="container">
+      <Profile avatarUrl={profile} username="Sharon Leshan" />
+      <span className="share" title="Share">
+        <img src={share} alt="share" />
+      </span>
+      <Links />
+      <Footer />
     </div>
   );
 }
