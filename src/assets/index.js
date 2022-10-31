63b30a5cf38faca93c7cdc0173b27eecbb4512d5
sharon-leshan <maleos380@gmail.com>
2022-10-31 15:04:24 +0300

    index.js

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+export { default as profile } from './images/profile.png';
+export { default as github } from './images/github.png';
+export { default as slack } from './images/slack.png';
+export { default as share } from './images/share.png';
+export { default as zuri } from './images/zuri.png';
+export { default as logo } from './images/I4G.png';
